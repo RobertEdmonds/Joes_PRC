114563e8eddd0b1201c30f9090d895649ef24cc5
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-12-17 12:17:56 -0700

    Connect apperral to sizes and validate for apperral

--- app/models/apperral.rb
+++ app/models/apperral.rb
@@ -1,2 +1,12 @@
 class Apperral < ApplicationRecord
+    validates :description, :price, {presence: true}
+    
+    has_many :xsmalls
+    has_many :smalls 
+    has_many :mediums 
+    has_many :larges  
+    has_many :xlarges
+    has_many :xxlarges 
+    has_many :orders 
+
 end
