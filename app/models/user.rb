044d4dbec99ba401917cdea7f5443303f00699c8
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-12-16 17:53:51 -0700

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    has_secured_password 
+
+    validates :email, presence: true, uniqueness: true
+    validates :first_name, :last_name, :state, :zip_code, :city, {presence: true} 
+    validates :phone_number, presence: true, uniqueness: true, numericality: { only_integer: true }, length: { minimum: 10, maximum: 12 }
+    validates :address, presence: true, uniqueness: true 
+    validates :password, confirmation: true 
+
+    has_many :orders
+    has_many :rifles, through: :orders 
+    has_many :apperrals, through: :orders 
 end
