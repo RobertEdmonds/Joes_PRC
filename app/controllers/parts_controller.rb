a624afa58fb3b6b2117f494148e2ca0fb7a239f7
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-01-09 12:57:50 -0500

    Add routes to parts

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,38 @@
 class PartsController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+    before_action :set_part, only: [:show, :update, :destroy]
+
+    def index 
+        render json: Part.all, status: :ok 
+    end
+
+    def show 
+        render json: @part, status: :ok
+    end
+
+    def create 
+        part = Part.create!(part_params)
+        render json: part, status: :created 
+    end
+
+    def update 
+        @part.update!(part_params)
+        render json: @part, status: :created 
+    end
+
+    def destroy 
+        @part.destroy 
+        head :no_content
+    end
+
+    private 
+
+    def part_params 
+        params.permit(:description, :inventory, :price)
+    end
+
+    def set_part 
+        @part = Part.find(params[:id])
+    end
+
 end
